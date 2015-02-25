f1375250f509451b435ac82306b3b9be8b2b0016
Claudia Kaiser <claudia.m.kaiser@gmail.com>
2015-02-25 16:12:00 -0600

    Webhooks Update1

--- app/controllers/WebhooksController.java
+++ app/controllers/WebhooksController.java
@@ -1,13 +1,28 @@
 package controllers;
 
+import play.Logger;
 import play.mvc.Result;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by clkaiser on 25/02/15.
  */
 public class WebhooksController extends Application {
 
-    public static Result initialSetup(String bTChallenge){
+    public static Result initialSetup(){
+
+        String bTChallenge="";
+
+        final Set<Map.Entry<String,String[]>> entries = request().queryString().entrySet();
+        for (Map.Entry<String,String[]> entry : entries) {
+            final String key = entry.getKey();
+            final String value = Arrays.toString(entry.getValue());
+
+            if (key.equals("bt_challenge")){bTChallenge = key;}
+        }
 
         return ok(currService.getWebhookVerificationResponse(bTChallenge));
     }
