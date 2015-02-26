3f4309ba5fcff839016f1af1afea985f51f2d84d
Claudia Kaiser <claudia.m.kaiser@gmail.com>
2015-02-25 23:28:33 -0600

    FileWriterTest1

--- app/controllers/WebhooksController.java
+++ app/controllers/WebhooksController.java
@@ -3,7 +3,10 @@ package controllers;
 import play.Logger;
 import play.mvc.Result;
 
-import java.util.Map;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * Created by clkaiser on 25/02/15.
@@ -25,11 +28,41 @@ public class WebhooksController extends Application {
 
     public static Result listener(){
 
-        String bt_signature = request().body().asJson().findValue("bt_signature").asText();
-        String bt_payload = request().body().asJson().findValue("bt_payload").asText();
+        //String bt_signature = request().body().asJson().findValue("bt_signature").asText();
+        //String bt_payload = request().body().asJson().findValue("bt_payload").asText();
 
-        Logger.debug("bt_signature: " + bt_signature + "bt_payload: " + bt_payload);
+        writeToWebhookLogFile(request().toString());
 
         return ok();
+    }
+
+    public static void writeToWebhookLogFile(String notification){
+
+        try{
+            String fileSeparator = System.getProperty("file.separator");
+            //Specify the file name and path here
+            File file =new File("logs" + fileSeparator + "webhooks.log");
+
+    	/* This logic is to create the file if the
+    	 * file is not already present
+    	 */
+            if(!file.exists()){
+                file.createNewFile();
+            }
+
+            //Here true is to append the content to file
+            FileWriter fw = new FileWriter(file,true);
+            //BufferedWriter writer give better performance
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(notification);
+            bw.newLine();
+            //Closing BufferedWriter Stream
+            bw.close();
+
+            Logger.info("Data successfully appended at the end of file");
+
+        }catch(IOException ioe){
+            Logger.error("Exception occurred:");
+        }
     }
 }
