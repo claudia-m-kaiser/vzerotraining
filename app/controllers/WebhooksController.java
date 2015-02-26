ce7d47aa59cfd2f173c4912e865d936ca61f0ae9
Claudia Kaiser <claudia.m.kaiser@gmail.com>
2015-02-25 22:15:42 -0600

    Listner Update1

--- app/controllers/WebhooksController.java
+++ app/controllers/WebhooksController.java
@@ -3,9 +3,7 @@ package controllers;
 import play.Logger;
 import play.mvc.Result;
 
-import java.util.Arrays;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * Created by clkaiser on 25/02/15.
@@ -27,12 +25,8 @@ public class WebhooksController extends Application {
 
     public static Result listener(){
 
-        final Map<String, String[]> params = request().body().asFormUrlEncoded();
-
-        Logger.debug("Request :" + request().toString());
-
-        String bt_signature = params.get("bt_signature")[0].toString();
-        String bt_payload = params.get("bt_payload")[0].toString();
+        String bt_signature = request().body().asJson().findValue("bt_signature").asText();
+        String bt_payload = request().body().asJson().findValue("bt_payload").asText();
 
         Logger.debug("bt_signature: " + bt_signature + "bt_payload: " + bt_payload);
 
